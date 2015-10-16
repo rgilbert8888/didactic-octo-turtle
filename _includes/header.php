e87666ae99a5bcf46b4b710b59eba366bd9ea512
Robyn G <rgilbert@gmail.com>
2015-10-16 17:01:51 -0400

    adding some php

---
+++ _includes/header.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+	<title>Phoenix Equipment</title>
+	<link rel="stylesheet" type="text/css" href="styles/main.css" />
+</head>
+<body>
+<div class="page-wrap">
+	<header>
+		<div class="top-logo">
+			<a href="index.html"><img src="images/phoenix_logo.jpeg" alt="pxlogo" /></a>
+		</div>
+		
+		<nav>
+			<div class="is-mobile js-menu">MENU</div>
+			<ul class="nav-main">
+				<li><a href="about.html">About</a></li>
+				<li>
+					<a href="#">Parts</a>
+					<ul class="nav-sub">
+						<li><a href="#">Skid Steer and Loader Tires</a></li>
+						<li><a href="#">Excavators</a></li>
+						<li><a href="#">Dozers</a></li>
+						<li><a href="#">Rubber Tracks</a></li>
+					</ul>
+				</li>
+				<li>
+					<a href="#">Menu Item</a>
+					<ul class="nav-sub">
+						<li><a href="#">Skid Steer and Loader Tires</a></li>
+						<li><a href="#">Excavators</a></li>
+						<li><a href="#">Dozers</a></li>
+						<li><a href="#">Rubber Tracks</a></li>
+					</ul>
+				</li>				
+				<li><a href="#">Contact</a></li>
+			</ul>
+		</nav>
+	</header>
--- index.php
+++ index.php
@@ -1,42 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Phoenix Equipment</title>
-	<link rel="stylesheet" type="text/css" href="styles/main.css" />
-</head>
-<body>
-<div class="page-wrap">
-	<header>
-		<div class="top-logo">
-			<a href="index.html"><img src="images/phoenix_logo.jpeg" alt="pxlogo" /></a>
-		</div>
-		
-		<nav>
-			<div class="is-mobile js-menu">MENU</div>
-			<ul class="nav-main">
-				<li><a href="about.html">About</a></li>
-				<li>
-					<a href="#">Parts</a>
-					<ul class="nav-sub">
-						<li><a href="#">Skid Steer and Loader Tires</a></li>
-						<li><a href="#">Excavators</a></li>
-						<li><a href="#">Dozers</a></li>
-						<li><a href="#">Rubber Tracks</a></li>
-					</ul>
-				</li>
-				<li>
-					<a href="#">Menu Item</a>
-					<ul class="nav-sub">
-						<li><a href="#">Skid Steer and Loader Tires</a></li>
-						<li><a href="#">Excavators</a></li>
-						<li><a href="#">Dozers</a></li>
-						<li><a href="#">Rubber Tracks</a></li>
-					</ul>
-				</li>				
-				<li><a href="#">Contact</a></li>
-			</ul>
-		</nav>
-	</header>
+<?php include('_includes/header.php') ?>
+
+<!-- <!DOCTYPE html> -->
+
 	<div class="content">
 		<div class="main">
 			<div class="col">
@@ -71,15 +36,9 @@
 		<div class="aside"></div>
 	</div>
 </div>
-	<footer class="sticky-footer">
-		<address>
-		  <p>Phoenix Equipment Parts</p>
-		  <p><a class="wsite-social-item wsite-social-mail" href="mailto:artseeger14@gmail.com" target="_blank"><span>Email Us</span></a></p>
-		  <p>842 Cody Road<br />Toms River, NJ  08753</p>
-		  <p>732-766-2109</p>
-		</address>
-	</footer>
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<?php include('_includes/footer.php') ?>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.0/jquery-ui.min.js"></script>
 	<script type="text/javascript">
 		$(document).on('ready', function(){
